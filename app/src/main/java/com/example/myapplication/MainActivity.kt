d315d5aa52b23400139a799a2603801930a6c76e
Onur Can Erdem <onure7@icloud.com>
2023-10-23 14:52:47 +0300

    login form example

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,63 +3,52 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+            LoginForm()
             }
+        }
+    }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginForm(){
+    var text by remember { mutableStateOf("Hello") }
+    Column {
+        Image(painter = painterResource(id = R.drawable.logo), contentDescription = "Ali")
+        Row {
+            Text(text = "username")
+            TextField(value = "", onValueChange = {text=it})
+        }
+        Row {
+            Text(text = "password")
+            TextField(value = "", onValueChange = {text=it})
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Login")
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-                .width(200.dp)
-                .height(100.dp)
-                .padding(10.dp)
-                .clickable { },
-        style = TextStyle(
-            textAlign = TextAlign.Right,
-            color = MaterialTheme.colorScheme.error,
-        ),
-    )
-}
-@Composable
-fun GreetingButton(){
-    Button(onClick = { /*TODO*/ }) {
-        Greeting(name = "Ali Duru")
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        GreetingButton()
-    }
+fun LoginFormPreview() {
+    LoginForm()
 }
